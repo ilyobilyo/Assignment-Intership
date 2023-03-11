ae12257f98199b8125c5586e7a9b3e9bedadfcb6
ilyobilyo <ilito20000@gmail.com>
2023-03-11 14:35:40 +0200

    Add constants for task stauses

---
+++ Assignment Intership/Constants/TaskStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment_Intership.Constants
+{
+    public static class TaskStatus
+    {
+        public const string Start = "Start";
+        public const string InProgress = "InProgress";
+        public const string Completed = "Completed";
+    }
+}
